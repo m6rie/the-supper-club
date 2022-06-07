fdf58241f4639450a55abee9dc2f102c923f701f
Marie Etong <m6rief@gmail.com>
2022-06-07 11:58:00 +0100

    part of the routes, controllers and view files

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,25 @@
+class PartiesController < ApplicationController
+  def index
+    @parties = Party.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+end
