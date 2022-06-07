83e69cf7e0baeb7065430f8e8ebd403305c0182b
Maria Casado <casado_me@hotmail.com>
2022-06-07 14:20:19 +0100

    fixing migration order

--- db/migrate/20220607111220_create_party_dates.rb
+++ db/migrate/20220607111220_create_party_dates.rb
@@ -3,7 +3,7 @@ class CreatePartyDates < ActiveRecord::Migration[7.0]
     create_table :party_dates do |t|
       t.integer :date
       t.string :votes
-
+      t.references :parties, null: false, foreign_key: true
       t.timestamps
     end
   end
