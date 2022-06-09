e4d99bf40e33ffc2ee1b5fba3329465cd9f5af53
Maria Casado <casado_me@hotmail.com>
2022-06-09 15:00:53 +0100

    worked on recipes' cards clikable to the recipes' website (no show page needed)

--- app/javascript/controllers/search_recipes_controller.js
+++ app/javascript/controllers/search_recipes_controller.js
@@ -2,21 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-recipes"
 export default class extends Controller {
-  static targets = ["form", "input", "content", "image"]
-  //  "health-label"
+  static targets = ["form", "input", "content"]
+
 
   search(event) {
 
-    console.log("hello")
-    console.log(this.formTarget)
-    console.log(this.inputTarget.value)
-    console.log(this.imageTarget)
+    // console.log("hello")
+    // console.log(this.formTarget)
+    // console.log(this.inputTarget.value)
+    // console.log(this.imageTarget)
     // console.log(this.contentTarget)
 
     const input = this.inputTarget.value
     const apiKey = '9a33f2a981e7930dfb3369195fc15726'
     const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=958988ca&app_key=${apiKey}`
-    // const results = document.querySelector("#results")
+
 
 
 
@@ -26,33 +26,26 @@ export default class extends Controller {
     .then((data) => {
       console.log(data.hits)
 
-      // const results = data.hits
-      // results.forEach ((recipe) => {
-      //   console.log(recipe)
-      // })
-
-
       data.hits.forEach ((recipe) => {
+        console.log(recipe)
 
-        // const contentTarget = `<div class="list-inline-item">
-        //   <img src="${result.healthLabels}" alt="">
-        //   <p>${result.Label}</p>
-        //   </div>`
-        // results.insertAdjacentHTML("beforeend", contentTarget)
-        // console.log(recipe['recipe']['image'])
+        this.contentTarget.insertAdjacentHTML("afterbegin",
+        `<a href="${recipe['recipe']['url']}" class="card border col-3">
+          <div class="row border">
+            <div class="col-12"><img src=${JSON.stringify(recipe['recipe']['image'])} width="100%"></div>
+          </div>
+          <div class="row border">
+            <div class="col-12">${JSON.stringify(recipe['recipe']['label'])}</div>
+          </div>
+          <div class="row border">
+            <p>Calories: </><div class="col-12">${JSON.stringify(Math.round(recipe['recipe']['calories']))}</div></p>
+          </div>
+          <div class="row border">
+            <p><div class="col-12">${JSON.stringify(recipe['recipe']['ingredientLines'].length)}</div> ingredients</p>
+          </div>
 
-        this.contentTarget.insertAdjacentHTML("beforeend", `<div class="col-6 border">${JSON.stringify(recipe['recipe']['label'])}</div>`)
-        this.healthTarget.insertAdjacentHTML("beforeend", `<div class="col-6 border">${JSON.stringify(recipe['recipe']['healthLabels'])}</div>`)
-        this.imageTarget.insertAdjacentHTML("beforeend", `<img src="${recipe['recipe']['image']}" alt="">`)
-
-        // this.health-labelTarget.insertAdjacentHTML("beforeend", `<div class="col-6 border">${JSON.stringify(recipe['healthLabel'])}</div>`)
-
-      })
-      // this.imageTarget.insertAdjacentHTML("beforeend", `<div class="col-4 border">${recipe['image']}</div>`)
-      // data['hits'].forEach ((recipe) => {
-      //   this.contentTarget.insertAdjacentHTML("beforeend", `<div class="col-6 border">${JSON.stringify(recipe)}</div>`)
-      //   this.allergentsTarget.insertAdjacentHTML("beforeend", `<div class="col-6 border">${JSON.stringify(recipe)}</div>`)
-      // })
+        </a>`
+      )})
     })
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :parties
 
+
+  get 'my_recipes', to: 'recipes#my_recipes'
+
   resources :recipes, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
