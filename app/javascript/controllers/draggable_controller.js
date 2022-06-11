4ca3375764ef03dc4e41aefa0735797b74abb7c4
Maria Casado <casado_me@hotmail.com>
2022-06-11 12:04:13 +0100

    worked on draggable option with different api searches

--- app/javascript/controllers/draggable_controller.js
+++ app/javascript/controllers/draggable_controller.js
@@ -1,8 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
-
+import Sortable from 'sortablejs';
 // Connects to data-controller="draggable"
 export default class extends Controller {
+  static targets = ["drag"]
+
   connect() {
-    console.log("hello")
+    // console.log(this.dragTarget)
+
+    const test = document.querySelector("#ourlist")
+    console.log(test)
+
+    new Sortable(test, {
+      group: "shared",
+      sort: false,
+      swap: true,
+      fallbackOnBody: false,
+      pull: 'clone',
+      put: ["recipes", "empty"],
+      onEnd(event){
+        console.log(event)
+      }
+
+
+    });
+
+    const empty = document.querySelector("#emptyspace")
+    console.log(empty)
+
+    new Sortable(empty, {
+      group: "shared",
+      sort: true,
+      swap: true,
+      put: ["recipes", "empty"],
+      onEnd(event){
+        console.log(event)
+      }
+    });
+
+    }
+
+
   }
-}
