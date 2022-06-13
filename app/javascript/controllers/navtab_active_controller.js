055e1d0cd0c70649e05cc44d02fad90f1bc26cd2
Maria Casado <casado_me@hotmail.com>
2022-06-13 12:49:33 +0100

    deleting something in nav_bar

--- app/javascript/controllers/navtab_active_controller.js
+++ app/javascript/controllers/navtab_active_controller.js
@@ -1,32 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navtab-active"
-// export default class extends Controller {
-//   static targets = ["navtabActive", "pages"]
-//   active() {
-//     // console.log("test")
-//     this.navtabActiveTarget
-//   }
-
-//   static targets = [""]
-//   connect() {
-//     console.log(this.pagesTarget)
-//   }
-// }
 
 export default class extends Controller {
 
   }
-
-  next() {
-    $(document).ready(function() {
-      $(document).foundation();
-      $('.next-tab').click(function() {
-          $('.tabs li.is-active').next().children('a').click();
-        });
-      }
-
-            //   $('.prev-tab').click(function() {
-            //     $('.tabs li.is-active').prev().children('a').click();
-            //   });
-            // });
