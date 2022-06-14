687acc75fd3017f2a7d4eaa652696351aefd477c
Marie Etong <m6rief@gmail.com>
2022-06-14 17:54:34 +0100

    ingredient page PDFable

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.3"
 
+#WICKED GEM
+gem 'wicked_pdf'
+gem "wkhtmltopdf-binary", group: :development
+gem "wkhtmltopdf-heroku", group: :production
+
+
 gem 'draggable-rails'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,9 @@
+require "rqrcode"
+require "chunky_png"
+
 class PartiesController < ApplicationController
   before_action :set_party, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: :invite
 
   def index
     @parties = Party.all
@@ -8,34 +12,83 @@ class PartiesController < ApplicationController
   def show
     @recipes = Recipe.all
     @party_recipe = Recipe.where(user_id: @party.id)
+
+    qrcode = RQRCode::QRCode.new("https://supperclub.pro/parties/#{@party.id}/invite")
+
+      png = qrcode.as_png(
+        bit_depth: 1,
+        border_modules: 1,
+        color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+        color: "black",
+        file: nil,
+        fill: "white",
+        module_px_size: 10,
+        resize_exactly_to: false,
+        resize_gte_to: false,
+        size: 250
+      )
+
+      IO.binwrite("./app/assets/images/qr_code#{@party.title}.png", png.to_s)
   end
 
   def new
-    # @party_date = PartyDate.new
-    # @party_theme = PartyTheme.new
-    # @theme = Theme.new
-    # @party_theme = @theme
-    @recipes = Recipe.all
     @party = Party.new
   end
 
   def ingredients
     @party = Party.find(params[:party_id])
     @party_recipe = @party.recipes
-
     @party_recipe.each do |recipe|
-      @ingredients = recipe[:ingredients].split(",")
+    @ingredients = recipe.ingredients
+    @recipe_ingredient = RecipeIngredient.new
+    end
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render  pdf: "Ingredients",
+                template: "parties/ingredients",
+                formats: [:html],
+                disposition: :inline,
+                layout: 'pdf'
+      end
     end
   end
 
   def create
-    @party = Party.new(party_params)
-    @party.user_id = current_user.id
-    # @party_date = PartyDate.new
-    if @party.save
-      redirect_to party_path(@party)
-    else
-      render :new, status: :unprocessable_entity
+    # ALL RECIPES
+    @recipes = []
+    # SAVING ATTRIBUTES
+    number_of_recipes = params[:party][:recipes_data][:title].size
+    titles = params[:party][:recipes_data][:title]
+    photo_urls = params[:party][:recipes_data][:recipe_url]
+    # ingredients = params[:party][:recipes_data][:ingredients] ------ ingredients: ingredients[n - 1] ------ TBA -------
+    # BUILDING RECIPES
+    number_of_recipes.times do |n|
+      @recipes << Recipe.create(title: titles[n - 1], photo_url: photo_urls[n - 1], prep_time: 30, description: "Delicous recipe")
+    end
+
+    # CREATING PARTY
+    @party = Party.create(
+      user: current_user,
+      title: params[:party][:title],
+      address: params[:party][:address],
+      theme: params[:party][:theme],
+      date: params[:party][:date],
+      attendancy: params[:party][:attendancy],
+      appetizers: params[:party][:appetizers],
+      mains: params[:party][:mains],
+      desserts: params[:party][:desserts]
+    )
+
+    # CONNECTING PARTIES AND RECIPES
+    @recipes.each do |recipe|
+      PartyRecipe.create(party: @party, recipe: recipe)
+    end
+
+    # AJAX RESPONSE
+    respond_to do |format|
+      format.json { render :json => @recipes }
+      format.html { puts "I am html" }
     end
   end
 
@@ -53,10 +106,14 @@ class PartiesController < ApplicationController
   def destroy
   end
 
+  def invite
+    @party = Party.find(params[:party_id])
+  end
+
   private
 
   def party_params
-    params.require(:party).permit(:title, :address, :date, :theme, :attendancy, :appetizers, :mains, :desserts)
+    params.require(:party).permit(:title, :address, :date, :theme, :attendancy, :appetizers, :mains, :desserts, :recipes_data)
   end
 
   def set_party
