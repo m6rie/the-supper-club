545b8df6e49dd2799b326ecf54ef9f3f68c8e927
filipe bernardo <filipebernardo.fb@gmail.com>
2022-06-14 14:23:58 +0100

    Created Convo Table with Topics and Subjects, Also made a link between QRCode and Invite Page

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,5 @@
+class ConversationsController < ApplicationController
+
+  def show
+  end
+end
---
+++ db/migrate/20220614095819_create_conversations.rb
@@ -0,0 +1,10 @@
+class CreateConversations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :conversations do |t|
+      t.string :topic
+      t.string :subject
+
+      t.timestamps
+    end
+  end
+end
