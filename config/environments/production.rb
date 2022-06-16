fb5dd9b586bd5c006ff2e27b502b7bb97943c14b
Maria Casado <casado_me@hotmail.com>
2022-06-16 12:47:11 +0100

    asssets compile false on fallback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
